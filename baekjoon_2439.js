153eacf7638116a9715ec3b31e9d4f4f116796da
hyoredm <hyoredm@gmail.com>
2022-09-30 23:59:47 +0900

    retry

---
+++ baekjoon_2439.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+let A = parseInt(input[0]);
+let star = '';
+
+for (let i = 0; i < A; i++) {
+    for (let j = 0; j < A - i - 1; j++) {
+        star = ' ';
+    }
+
+    for (let k = 0; k <= i; k++) {
+        star += '*';
+    }
+    star += '\n';
+}
+console.log(star);
